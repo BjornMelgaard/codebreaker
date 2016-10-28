2acbd975d5919823191f1ba889ab1e91ebb044eb
BjornMelgaard <melgaardbjorn@gmail.com>
2016-10-28 16:58:02 +0300

    specs refactoring

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,21 +4,39 @@ module Codebreaker
   describe Game do
     let(:game) { Game.new }
 
-    describe '#secret_code' do
-      subject { game.instance_variable_get :@secret }
-      it { expect(subject).to be_a_kind_of(String) }
-      it { expect(subject.length).to eq(4) }
-    end
-
     describe '#guess' do
       before do
-        game.instance_variable_set(:@secret, '1111')
+        game.instance_variable_set(:@secret, '1234')
       end
 
-      it 'don\'t execute if no attempts left' do
-        game.instance_variable_set(:@attempts_left, 0)
-        expect(game.guess('0000')).to be_nil
+      it 'should iterate game' do
+        game.guess('1043')
+        expect(game.win?).to be   false
+        expect(game.loose?).to be false
+        expect(game.ended?).to be false
       end
+
+      it 'set win state' do
+        game.guess('1234')
+        expect(game.win?).to be   true
+        expect(game.loose?).to be false
+        expect(game.ended?).to be true
+      end
+
+      it 'set loose state' do
+        game.instance_variable_set(:@attempts_left, 0)
+        game.guess('0000')
+        expect(game.win?).to be   false
+        expect(game.loose?).to be true
+        expect(game.ended?).to be true
+      end
+    end
+
+    it '#secret_code should show secret only if game ended' do
+      expect(game.secret_code).to be nil
+      allow(game).to receive(:ended?).and_return true
+      expect(game.secret_code).to be_a_kind_of(String)
+      expect(game.secret_code.length).to eq(4)
     end
   end
 end
